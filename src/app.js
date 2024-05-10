cd01d80f7065cf69503f2421531c5b05afa3f2e8
Francisco Zuidwijk <electrocisko@gmail.com>
2024-05-10 14:14:01 -0300

    configurando app

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
-console.log("Monkey");
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import dotenvConfig from "./config/dotenvConfig.js";
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const PORT = dotenvConfig.app.PORT;
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+
+app.get("/", (req,res) => {
+    res.send("<h1>Servidor APP</h1>")
+})
+
+
+const server = app.listen(PORT, () => {
+    console.log(`Server levantado en http://localhost:${PORT}`);
+})
